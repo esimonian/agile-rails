a3a962d45983c7169fb42413f5c38ef47cd4bc6c
esimonian <eli.simonian@gmail.com>
2017-04-10 09:19:05 -0700

    cart set up

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,18 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    @session_count = session_index_count
   end
+
+  private 
+
+  def session_index_count
+    if session[:count].nil?
+      session[:count] = 0
+    else 
+      session[:count] += 1
+    end  
+  end
+
 end
