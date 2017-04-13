2c0d965e2e53500900318f7a705daf58f301cd61
esimonian <eli.simonian@gmail.com>
2017-04-13 09:07:38 -0700

    add devise

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
+gem 'bourbon'
 gem 'materialize-sass'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
@@ -35,6 +35,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem "faker"
+gem 'devise'
+gem "administrate"
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    resources :carts
+    resources :line_items
+    resources :orders
+    resources :products
+
+    root to: "users#index"
+  end
+
   resources :orders
   resources :line_items
   resources :carts
@@ -11,5 +21,9 @@ Rails.application.routes.draw do
     put 'decrease', on: :member
     put 'increase', on: :member
   end
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
